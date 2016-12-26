7a32d03903b932642f99513a407ee6531c03cae3
Christian Reuter <christian@aoe-maniac.de>
2016-12-26 19:16:28 +0100

    Fixed testscript.

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 copy build\Debug\MPShmup.exe Deployment\Test.exe
 cd Deployment
-start Test.exe [27734]
+start Test.exe 27734
 timeout /t 3 /nobreak
 start Test.exe [27403 localhost 27734]
 start Test.exe [27404 localhost 27734]
